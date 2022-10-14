976bf63292f701283337d8caff9fc427f9121d3e
Jocelyn Ng <jng@hubspot.com>
2022-10-14 15:25:21 +0800

    session 2

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const app = express();
-
+app.use(express.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
     res.render('home');
-  });
+});
 
-app.listen(3000, function()
-  {console.log("Server started on port 3000");}
-);
+app.post('/', (req, res) => {
+});
+
+const MongoClient = require('mongodb').MongoClient;
+
+const CONNECTION_URL = "mongodb+srv://joceyng:test@joceyng.hqak4.mongodb.net/test?retryWrites=true&w=majority";
+const DATABASE_NAME = "joceyng"; // you can change the database name
+var database, collection;
+
+MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
+  if(error) throw error;
+
+  database = client.db(DATABASE_NAME);
+  collection = database.collection("testcollection"); // you can change the collection name
+
+  // Start the application after the database connection is ready
+  app.listen(port, () => {
+    console.log('This app is running on port 3000')
+  });
+});
